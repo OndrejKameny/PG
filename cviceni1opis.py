9bb075c233d2ef8c961bc1893db3dd19cc1826f0
OndrejKameny <kameno01@jcu.cz>
2025-10-02 08:58:44 +0000

    prvni uloha

---
+++ cviceni1opis.py
@@ -0,0 +1,43 @@
+
+
+
+
+def main():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+def main():
+    vstup = input("Zadej cislo:")
+    print(vstup)
+
+
+
+    def main():
+    vstup = input("Zadej cislo: ")
+    vstup = int(vstup)
+    vstup *=3
+    print(f"trojnsobek cisla je:(vstup)")
+
+
+
+def main():
+    vstup = input("Zadej cislo: ")
+    vstup = int(vstup)
+    vstup *= 3
+    print(f"trojnsobek cisla je:(vstup)")
+    vstup -= 2
+    if vstup > 10 
+        print("vetsi  nez 10")
+    else:
+        print("mensi nez 10")
+    
+
+
+
+if __name__ == "__main__":
+    main()
