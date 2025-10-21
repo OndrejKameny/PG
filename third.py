916d93ffd828fc11df3aaa59592482646156ed85
OndrejKameny <kameno01@jcu.cz>
2025-10-21 18:54:00 +0000

    úkol 3

--- third.py
+++ third.py
@@ -1,19 +1,19 @@
 def je_prvocislo(cislo):
-    if cislo<2:
+    if cislo < 2:
         return False
     for i in range(2, int(cislo ** 0.5) + 1):
-        if cislo%i == 0:
+        if cislo % i == 0:
             return False
     return True
 
-def vrat_prvocisla(maximum):
+def vrat_prvocisla(cislo2):
     prvocisla=[]
-    for i in range(2, maximum + 1):
+    for i in range(2, cislo2 + 1):
         if je_prvocislo(i):
             prvocisla.append(i)
     return prvocisla
 
 if __name__ == "__main__":
-    maximum=int(input("Zadej maximum: "))
-    prvocisla=vrat_prvocisla(maximum)
+    cislo2 = int(input("Zadej maximum: "))
+    prvocisla = vrat_prvocisla(cislo2)
     print(prvocisla)
