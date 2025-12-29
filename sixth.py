e0bdd4f8d20dac17fdf696906ad99238a9322b94
OndrejKameny <kameno01@jcu.cz>
2025-12-29 14:59:13 +0000

    úkol 6 oprava

--- sixth.py
+++ sixth.py
@@ -1,16 +1,31 @@
 import sys
 import requests
 import re
+from urllib.parse import urljoin
 
 def download_url_and_get_all_hrefs(url):
     response = requests.get(url)
     content = response.text
-    hrefs = re.findall(r'href=["\'](.*?)["\']', content)
+
+    raw_hrefs = re.findall(r'href\s*=\s*"([^"]+)"', content)
+
     hrefs = []
+    for href in raw_hrefs:
+        if not href:
+            continue
+        if href.startswith("#"):
+            continue
+        
+        full_url = urljoin(url, href)
+        hrefs.append(full_url)
+
     return hrefs
 
 if __name__ == "__main__":
     url = input("Zadej URL:")
+
+    if not url.startswith("https://") and not url.startswith("https://"):
+        url = "https://" + url
     try:
         links = download_url_and_get_all_hrefs(url)
         print("Nalezené odkazy:")
